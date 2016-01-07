916097a84cb1bd10ae7916b84dd029cfd06704d3
Samuel Sekiwere <sekiskylink@gmail.com>
2016-01-07 18:31:39 +0300

    add webhooks for mtrackpro

---
+++ webhooks/mtracpro_webooks.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Sekiwere Samuel"
+
+import web
+# import psycopg2
+# import psycopg2.extras
+import logging
+import requests
+import json
+import parsedatetime
+import datetime
+import re
+from settings import conf, MAPPING, XML_TEMPLATE, DEFAULT_DATA_VALUES  # conf is a dictionary with our settings
+
+urls = (
+    "/cases", "Cases",
+    "/deaths", "Deaths",
+    "/dhis2queue", "Dhis2Queue",
+    "/test", "Test",
+)
+app = web.application(urls, globals(), True)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s [%(process)d] %(levelname)-4s:  %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='/var/log/webhooks/access.log',
+    filemode='a')
+cal = parsedatetime.Calendar()
+# Helper functions
+
+API_URL = 'http://localhost:8000/api/v1/'
+CASES_POSITIONS = {
+    'ma': 0, 'dy': 1, 'sa': 2, 'af': 3, 'ae': 4, 'ab': 5, 'mg': 6, 'ch': 7, 'gw': 8,
+    'me': 9, 'nt': 10, 'vf': 11, 'pl': 12, 'tf': 13, 'yf': 14, 'tb': 15, 'md': 16, 'pd': 17
+}
+DELIMITER = '.'
+KEYWORDS_DATA_LENGTH = {
+    'cases': 16,
+    'death': 18,
+}
+
+
+def get_reporting_week(date):
+    """Given date, return the reporting week in the format 2016W01
+    reports coming in this week are for previous one.
+    """
+    offset_from_last_sunday = date.weekday() + 1
+    last_sunday = date - datetime.timedelta(days=offset_from_last_sunday)
+    year = last_sunday.year
+    weeknum = int(last_sunday.strftime("%W")) + 1
+    return "%sW%d" % (year, weeknum)
+
+
+def parse_message(msg, kw=""):
+    msg = msg.strip().lower()
+    separators = []
+    segments = []
+    separator = DELIMITER or '\\s'
+    search_str = '[%s]+' % separator
+    match = re.search(search_str, msg)
+    while match:
+        segment = msg[0:match.start()]
+        separator = msg[match.start():match.end()]
+        segments.append(segment)
+        separators.append(separator)
+        msg = msg[match.end():]
+        match = re.search(search_str, msg)
+    segments.append(msg)
+
+    # remove empty segments
+    stripped_segments = []
+    for segment in segments:
+        segment = segment.strip()
+        if len(segment):
+            stripped_segments.append(segment)
+
+    # do more cleaning here for stuff like ma2 instead of ma.2
+
+    if not len(segments) % 2 == 0:
+        return "not all commands have a value"
+    command_value_pairs = []
+    dummy_resp = ['0' for i in range(KEYWORDS_DATA_LENGTH[kw])]
+
+    for idx, segment in enumerate(segments):
+        if segment in CASES_POSITIONS:
+            cmd = segment
+            if (idx + 1) <= (len(segments) - 1):
+                val = segments[idx + 1]
+                dummy_resp[CASES_POSITIONS[segment]] = val
+            else:
+                # return an error
+                val = 0
+            command_value_pairs.append((cmd, val))
+        else:
+            continue
+    print segments
+    return '.'.join(dummy_resp)
+
+
+def post_request(data, url=conf['default_api_uri']):
+    response = requests.post(url, data=data, headers={
+        'Content-type': 'application/json',
+        'Authorization': 'Token %s' % conf['api_token']})
+    return response
+
+
+def get_request(url):
+    response = requests.get(url, headers={
+        'Content-type': 'application/json',
+        'Authorization': 'Token %s' % conf['api_token']})
+    return response
+
+
+def get_available_fields():
+    response = requests.get(
+        '%sfields.json' % API_URL, headers={
+            'Content-type': 'application/json',
+            'Authorization': 'Token %s' % conf['api_token']})
+    results = [k['key'] for k in json.loads(response.text)['results']]
+    return results
+
+
+def add_reporter_fields():
+    our_fields = [
+        {'label': 'facility', 'value_type': 'T'},
+        {'label': 'facilityuuid', 'value_type': 'T'},
+        {'label': 'district', 'value_type': 'I'},
+        {'label': 'village', 'value_type': 'T'},
+    ]
+    fields = get_available_fields()
+    for f in our_fields:
+        if f['label'] not in fields:
+            res = post_request(
+                json.dumps(f), '%sfields.json' % API_URL)
+            print res.text
+
+
+def push_reporters():
+    # with open('test.csv') as f:
+    with open('reporters.csv') as f:
+        for l in f:
+            fields = {}
+            val = l.strip().split('#')
+            name = val[0] if val[0] != '#' else ''
+            phone = '+' + val[1]
+            if len(phone) >= 12:
+                fields['district'] = val[2]
+                fields['facility'] = val[3] if val[3] != '#' else ''
+                fields['facilityuuid'] = val[6] if val[6] != '#' else ''
+                data = {
+                    "name": name, "phone": phone, "language": "eng",
+                    "groups": val[4].split(","),
+                    "fields": fields
+                }
+                post_data = json.dumps(data)
+                res = post_request(post_data)
+                # print post_data
+                print res.text
+
+
+class Cases:
+    def GET(self):
+        return json.dumps({"message": "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0"})
+
+    def POST(self):
+        params = web.input()
+        values = json.loads(params['values'])  # only way we can get out Rapidpro values in webpy
+        msg_list = [v.get('value') for v in values if v.get('label') == 'msg']
+        if msg_list:
+            msg = msg_list[0]
+            if msg.startswith('.'):
+                msg = msg[1:]
+        # print msg
+        message = parse_message(msg, 'cases')
+
+        return json.dumps({"message": message})
+
+
+class Deaths:
+    def GET(self):
+        return json.dumps({"message": "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0"})
+
+    def POST(self):
+        params = web.input()
+        values = json.loads(params['values'])  # only way we can get out Rapidpro values in webpy
+        msg_list = [v.get('value') for v in values if v.get('label') == 'msg']
+        if msg_list:
+            msg = msg_list[0]
+            if msg.startswith('.'):
+                msg = msg[1:]
+        # print msg
+        message = parse_message(msg, 'death')
+
+        return json.dumps({"message": message})
+
+
+class Test:
+    def GET(self):
+        return json.dumps({"message": "1.2.3"})
+
+    def POST(self):
+        return json.dumps({"message": "1+0+3+4+5+6+7+8+9+10+11+12+13+14+15+16.0.0"})
+
+
+class Dhis2Queue:
+    def GET(self):
+        return json.dumps({"status": "success"})
+
+    def POST(self):
+        params = web.input(facilityuuid="", form="")
+        print params.facilityuuid
+        print params.phone
+        values = json.loads(params['values'])  # only way we can get out Rapidpro values in webpy
+        dataValues = ""
+        if params.facilityuuid:
+            for v in values:
+                val = v.get('value')
+                try:
+                    val = int(float(val))
+                except:
+                    pass
+                label = v.get('label')
+                if val and val.__str__().isdigit():
+                    slug = "%s_%s" % (params.form, label)
+                    print "%s=>%s" % (slug, val), MAPPING[slug]
+                    dataValues += (
+                        "<dataValue dataElement='%s' categoryOptionCombo="
+                        "'%s' value='%s' />\n" %
+                        (MAPPING[slug]['dhis2_uuid'], MAPPING[slug]['dhis2_combo_id'], val))
+            if not dataValues and params.form in ('cases', 'death'):
+                dataValues = DEFAULT_DATA_VALUES[params.form]
+
+            if dataValues:
+                args_dict = {
+                    'complete_date': datetime.datetime.now().strftime("YYYY-MM-DD"),
+                    'period': get_reporting_week(datetime.datetime.now()),
+                    'orgunit': params.facilityuuid,
+                    'datavales': dataValues
+                }
+
+                post_xml = XML_TEMPLATE % args_dict
+                print post_xml
+                # now ready to queue to DB for pushing to DHIS2
+
+        return json.dumps({"status": "success"})
+
+
+# print get_available_fields()
+# add_reporter_fields()
+# push_reporters()
+
+if __name__ == "__main__":
+    app.run()
+application = app.wsgifunc()
+wsgiapp = app.wsgifunc()
---
+++ webhooks/settings.py
@@ -0,0 +1,337 @@
+conf = {
+    # 'api_token': '4b61f2fb79d095ad428c1850ae091f1e993a4b4e',
+    'api_token': 'c8cde9dbbdda6f544018e9321d017e909b28ec51',
+    'default_api_uri': 'http://localhost:8000/api/v1/contacts.json'
+}
+
+MAPPING = {
+    'apt_emtct_expected': {
+        'descr': 'Expected eMTCT Mothers on Appointment',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'xVpHSWaIBEX'
+    },
+    'apt_emtct_missed': {
+        'descr': 'eMTCT Missed Appointments',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'LYT11E96lC5'
+    },
+    'apt_opd_new': {
+        'descr': 'OPD New Attendees',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'xqSBj2KUlas'
+    },
+    'apt_opd_total': {
+        'descr': 'OPT Total Attendence',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'xb4g6Zf7fAT'
+    },
+    'arv_emtct': {
+        'descr': 'ARVs (Fixed - DC eMTCT)',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'xgSJLvdm2cQ'
+    },
+    'arv_nevirapine': {
+        'descr': 'Nevirapine Therapy',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'JPVp2yLEimL'
+    },
+    'arv_screening': {
+        'descr': 'HIV screening test kits',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'kteQimFQJom'
+    },
+    'cases_ab': {
+        'descr': 'Cases of Animal Bites',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'mdIPCPfqXaJ'},
+    'cases_ae': {
+        'descr': 'Cases of Adverse Events Following Immunization',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'fTwT8uX9Uto'
+    },
+    'cases_af': {
+        'descr': 'Cases of Acute Flacid Paralysis',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'U7cokRIptxu'
+    },
+    'cases_ch': {
+        'descr': 'Cases of Cholera',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'FPsN2bDpRru'
+    },
+    'cases_dy': {
+        'descr': 'Cases of Dysentery',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'NoJfAIcxjSY'
+    },
+    'cases_gw': {
+        'descr': 'Cases of Guinea Worm',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'hzGgkiOjfRs'
+    },
+    'cases_id': {
+        'descr': 'Cases of Other Emerging Infectious Diseases',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'ZZrhG7CYNjy'
+    },
+    'cases_ma': {
+        'descr': 'Cases of Malaria',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'fclvwNhzu7d'
+    },
+    'cases_me': {
+        'descr': 'Cases of Measles',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'v8HuPNb03yq'
+    },
+    'cases_mg': {
+        'descr': 'Cases of Bacterial Meningitis',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'JxUexqKeXtZ'},
+    'cases_nt': {
+        'descr': 'Cases of Neonatal Tetanus',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'euf2TqlswFp'
+    },
+    'cases_pl': {
+        'descr': 'Cases of Plague',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'peyFcHisgZB'
+    },
+    'cases_rb': {
+        'descr': 'Cases of Rabies',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'tTGAElxDIWg'
+    },
+    'cases_sa': {
+        'descr': 'Cases of Severe Acute Respiratory Infection',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'MHtpmIQEnyc'
+    },
+    'cases_tb': {
+        'descr': 'Presumptive Multi Drug Resistance (MDR) TB',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'yzdPKhvgeXq'},
+    'cases_tf': {
+        'descr': 'Cases of Typhoid Fever',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'IkI01xB7RIi'
+    },
+    'cases_vf': {
+        'descr': 'Cases of Other Viral Hemorrhagic Fevers',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'BMJMdxh1Loy'
+    },
+    'cases_yf': {
+        'descr': 'Cases of Yellow Fever',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'OE8baEzA2Cq'
+    },
+    'death_ab': {
+        'descr': 'Deaths from Animal Bites',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'UKYPpKnSBK4'
+    },
+    'death_ae': {
+        'descr': 'Deaths from Adverse Events Following Immunization',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'k2dUb4N7D0D'
+    },
+    'death_af': {
+        'descr': 'Deaths from Acute Flacid Paralysis',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'XzYd9m4XaKS'
+    },
+    'death_ch': {
+        'descr': 'Deaths from Cholera',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'QP9yftzrkne'
+    },
+    'death_dy': {
+        'descr': 'Deaths from Dysentery',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'JTc25LIvtQb'
+    },
+    'death_gw': {
+        'descr': 'Deaths from Guinea Worm',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'yvbPC5zDb3c'
+    },
+    'death_id': {
+        'descr': 'Deaths from Other Emerging Infectious Diseases',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'qHSErXOlcf3'
+    },
+    'death_ma': {
+        'descr': 'Deaths from Malaria',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'YXIu5CW9LPR'
+    },
+    'death_md': {
+        'descr': 'Maternal deaths',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'ck3jFjr8fOT'
+    },
+    'death_me': {
+        'descr': 'Deaths from Measles',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'tjTnFJ1QdVz'
+    },
+    'death_mg': {
+        'descr': 'Deaths from Bacterial Meningitis',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'r3xIBQaeLsT'},
+    'death_nt': {
+        'descr': 'Deaths from Neonatal Tetanus',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'tIh9TQxGOJU'
+    },
+    'death_pd': {
+        'descr': 'Perinatal deaths',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'nG5hrCX3vyP'
+    },
+    'death_pl': {
+        'descr': 'Deaths from Plague',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'hBcX7S7xBcu'
+    },
+    'death_rb': {
+        'descr': 'Deaths from Rabies',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'Dy90lwS6c6x'
+    },
+    'death_sa': {
+        'descr': 'Deaths from Severe Acute Respiratory Infection',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'qdZbdsCpIr7'
+    },
+    'death_tb': {
+        'descr': 'Presumptive Multi Drug Resistance (MDR) TB',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'eEruojUV6Jh'
+    },
+    'death_tf': {
+        'descr': 'Deaths from Typhoid Fever',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'OMxmmYvvLai'
+    },
+    'death_vf': {
+        'descr': 'Deaths from Other Viral Hemorrhagic Fevers',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'BX2PYPKm8F9'
+    },
+    'death_yf': {
+        'descr': 'Deaths from Yellow Fever',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'w3mO7SZdbb8'
+    },
+    'mat_mic_neg_treated': {
+        'descr': 'Microscopy negative cases treated',
+        'dhis2_combo_id': 'TTwU8Bv4fKm',
+        'dhis2_uuid': 'ZgPtploqq0L'
+    },
+    'mat_mic_pos': {
+        'descr': 'Microscopy positive cases',
+        'dhis2_combo_id': '3rYpFjLrrYa',
+        'dhis2_uuid': 'KPmTI3TGwZw'
+    },
+    'mat_mic_pos_treated': {
+        'descr': 'Microscopy positive cases treated',
+        'dhis2_combo_id': 'vp37EZVg3xG',
+        'dhis2_uuid': 'ZgPtploqq0L'
+    },
+    'mat_mic_tested': {
+        'descr': 'Microscopy tested cases',
+        'dhis2_combo_id': 't8Jv78lnSbU',
+        'dhis2_uuid': 'KPmTI3TGwZw'
+    },
+    'mat_not_treated': {
+        'descr': 'Not tested cases treated',
+        'dhis2_combo_id': 'w16OBdsg4b7',
+        'dhis2_uuid': 'ZgPtploqq0L'
+    },
+    'mat_rdt_neg_treated': {
+        'descr': 'RDT negative cases treated',
+        'dhis2_combo_id': 'NfJyyb1eBaw',
+        'dhis2_uuid': 'ZgPtploqq0L'
+    },
+    'mat_rdt_pos': {
+        'descr': 'RDT positive cases',
+        'dhis2_combo_id': 'PfdPCxOoiBj',
+        'dhis2_uuid': 'KPmTI3TGwZw'
+    },
+    'mat_rdt_pos_treated': {
+        'descr': 'RDT positive cases treated',
+        'dhis2_combo_id': 'QOWAmZ7jBno',
+        'dhis2_uuid': 'ZgPtploqq0L'
+    },
+    'mat_rdt_tesed': {
+        'descr': 'RDT tested cases',
+        'dhis2_combo_id': 'kcXY5cWAOsB',
+        'dhis2_uuid': 'KPmTI3TGwZw'
+    },
+    'mat_sus_ma': {
+        'descr': 'Suspected Malaria (fever)',
+        'dhis2_combo_id': '3W3d2AqT4Aq',
+        'dhis2_uuid': 'KPmTI3TGwZw'
+    },
+    'slug': {
+        'descr': 'description',
+        'dhis2_combo_id': 'dhis2_combo_id',
+        'dhis2_uuid': 'dhis2_uuid'
+    },
+    'tra_act': {
+        'descr': 'ACT (Tablets)',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'Ju0Cann41Ul'
+    },
+    'tra_amox': {
+        'descr': 'Amoxcilline',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'tFrWdigZ4oo'
+    },
+    'tra_depo': {
+        'descr': 'Depo - Provera',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'v6ACxYNPIvm'
+    },
+    'tra_fansidar': {
+        'descr': 'Fansidar',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'fX6A6NO18pY'
+    },
+    'tra_iv': {
+        'descr': 'IV artesunate',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'O2OrXuZo4hB'
+    },
+    'tra_me': {
+        'descr': 'Measles Vaccine',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'DgeykDdSLzI'
+    },
+    'tra_ors': {
+        'descr': 'ORS (Scakets)',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'zmimiDOJwaf'
+    },
+    'tra_rdt': {
+        'descr': 'Malaria RDTs',
+        'dhis2_combo_id': 'gGhClrV5odI',
+        'dhis2_uuid': 'CzsKMvQDwMV'
+    }
+}
+
+XML_TEMPLATE = """
+<dataValueSet xmlns="http://dhis2.org/schema/dxf/2.0" dataSet="V1kJRs8CtW4" completeDate="%(complete_date)s" period="%(period)s" orgUnitIdScheme="uuid" orgUnit="%(orgunit)s">
+<dataValues>
+    %(datavales)s
+</dataValue>
+</dataValueSet>
+"""
+
+DEFAULT_DATA_VALUES = {
+    'cases': "<dataValue dataElement='fclvwNhzu7d' categoryOptionCombo='gGhClrV5odI' value='0' />",
+    'death': "<dataValue dataElement='YXIu5CW9LPR' categoryOptionCombo='gGhClrV5odI' value='0' />"
+}
